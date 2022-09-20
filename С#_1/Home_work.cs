091e71739a9e2e2dc935840fe5b7db74f7be07a2
black_voodoo <developmest@gmail.com>
2022-09-20 09:44:00 +0300

    added homework from c# fisrst seminar

---
+++ С#_1/Home_work.cs
@@ -0,0 +1,54 @@
+/* Console.WriteLine("Введите число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine($"Максимальное число {num1}, минимальное число {num2}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число {num2}, минимальное число {num1}");
+}
+
+Console.WriteLine("Введите число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine());
+int max = num1;
+if (num2 > max)
+{
+    max = num2;
+}
+else if (num3 > max)
+{
+    max = num3;
+}
+Console.WriteLine($"Максимальное число {max}");
+
+
+Console.WriteLine("Введите число ");
+int num1 = int.Parse(Console.ReadLine());
+if (num1 % 2 == 0)
+{
+    Console.Write("Четное");
+}
+else
+{
+    Console.Write("Нечетное");
+}
+
+*/
+Console.WriteLine("Введите число ");
+int num1 = int.Parse(Console.ReadLine());
+int counter = 1;
+while (counter <= num1)
+{
+    if (counter % 2 == 0)
+    {
+        Console.Write($"{counter}, ");
+    }
+    counter += 1;
+}
--- С#_1/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int result = 3;
-int result2 = 6;
-Console.WriteLine($"Hello it is {result2} but no {result} ok {result}");
